24a8b43f22a4a753e91ce1e700d35c91ac4aa389
LaLoutre <laloutrecouillue@gmail.com>
2025-09-04 15:10:43 +0200

    wip

---
+++ AlarmClock/IView.cpp
@@ -0,0 +1,5 @@
+#include "iView.h"
+
+void IView::AddButton(lv_obj_t* parent, lv_event_cb_t callback, const char* text, ControlContext* controlContext){
+
+}
--- AlarmClock/IView.h
+++ AlarmClock/IView.h
@@ -1,11 +1,15 @@
 #ifndef IVIEW_H
 #define IVIEW_H
+#include "Arduino.h"
+#include "lvgl.h"
+#include "ControlContext.h"
 
 class IView {
 public:
     virtual void Update() = 0;  // Called every loop cycle to update logic
     virtual void Render(int width, int height, uint8_t param) = 0;  // Called to draw or print the UI
     virtual ~IView() {}         // Virtual destructor (important!)
+    void AddButton(lv_obj_t* parent, lv_event_cb_t callback, const char* text, ControlContext* controlContext);
 };
 
 #endif
